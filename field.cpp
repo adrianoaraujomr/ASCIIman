bd22ba9740a1732673d5e6ce837ca22438c64215
Adriano <adrianoshirost@gmail.com>
2019-08-21 15:09:36 -0400

    Trial 1.315

--- field.cpp
+++ field.cpp
@@ -8,7 +8,9 @@
 using namespace std;
 
 char field[MAX_LINES][MAX_COLUMNS];
-int player_pos[2] = {1,1};
+int player_pos[2] = {1,2};
+int bomb_clock = -1;
+int bomb_pos[2] = {1,1};
 
 void create_field(){
 
@@ -35,46 +37,66 @@ void create_field(){
 }
 
 void process_input(char pl_input){
-	int x = player_pos[0];
-	int y = player_pos[1];			
+	int a = player_pos[0]; //line coordenate
+	int b = player_pos[1]; //column coordenate
 
 	cout << pl_input << endl;
 
 	switch(pl_input){
-
 		case 'w' : 
-			y -= 1;
-			if(y > 0 && field[x][y] == ' ') player_pos[1] = y;
+			a -= 1;
+			if(a > 0 && field[a][b] == ' ') player_pos[0] = a;
 
 			break;
 		case 'a' :
-			x -= 1;
-			if(x > 0 && field[x][y] == ' ') player_pos[0] = x;
+			b -= 1;
+			if(b > 0 && field[a][b] == ' ') player_pos[1] = b;
 
 			break;
 		case 's' :
-			y += 1;
-			if(y < MAX_LINES && field[x][y] == ' ') player_pos[1] = y;
+			a += 1;
+			if(a < MAX_LINES && field[a][b] == ' ') player_pos[0] = a;
 
 			break;
 		case 'd' :
-			x += 1;
-			if(x < MAX_COLUMNS && field[x][y] == ' ') player_pos[0] = x;
+			b += 1;
+			if(b < MAX_COLUMNS && field[a][b] == ' ') player_pos[1] = b;
 
 			break;
 		case 'b' :
-			field[y][x] = 'o';
+			if(bomb_clock == -1) {
+				field[a][b] = 'o';
+				bomb_pos[0] = a;
+				bomb_pos[1] = b;
+				bomb_clock = 4; //240
+			}
 
 			break;
-
 	}
+
+	a = bomb_pos[0];
+	b = bomb_pos[1];
+
+	if(bomb_clock == 0){
+		bomb_clock = -1;
+		for(int l = a - 1;l < a + 2;l++){
+			for(int c = b - 1;c < b + 2;c++){
+				if(l == player_pos[0] && c == player_pos[b]) exit(1);
+				if(field[l][c] == '#' || field[l][c] == 'o') {
+					field[l][c] = ' ';
+				}
+			}
+		}
+	}
+
+	if(bomb_clock > 0) bomb_clock -= 1;
 }
 
 void display_field(){
 
 	for(int i{0};i < MAX_LINES;i++){
 		for(int j{0};j < MAX_COLUMNS;j++){
-			if(i == player_pos[1] && j == player_pos[0]) cout << 'A' << ' ';
+			if(i == player_pos[0] && j == player_pos[1]) cout << 'A' << ' ';
 			else cout << field[i][j] << ' ';
 		}
 		cout << endl;
@@ -82,12 +104,15 @@ void display_field(){
 }
 
 int main(){
+	char pl_input;
 
 	create_field();
 
 	while(1){
 		display_field();
-		char pl_input;
+		cout << "bomb_clock = " << bomb_clock << endl;
+		cout << "player_pos = (" << player_pos[0] << "," << player_pos[1] << ")" << endl;
+		cout << "bomb_pos = (" << bomb_pos[0] << "," << bomb_pos[1] << ")" << endl;
 
 		cin >> pl_input;
 		process_input(pl_input);
